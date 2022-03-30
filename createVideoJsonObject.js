5a65d7f375cfbb33728510e220bbf703da860457
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2022-03-30 14:31:42 +0200

    bei Download Datei überschreiben

--- createVideoJsonObject.js
+++ createVideoJsonObject.js
@@ -1,10 +1,10 @@
 //JSON fuer Videodatei erzeugen
 //ffprobe.exe muss vorhanden und im Path sein (z.B. im ffmpeg-bundle)
-const fs = require('fs-extra')
-const { getVideoDurationInSeconds } = require('get-video-duration')
+const fs = require("fs-extra");
+const { getVideoDurationInSeconds } = require("get-video-duration");
 
 //Zeit Formattierung laden: [5, 13, 22] => 05:13:22
-const timelite = require('timelite');
+const timelite = require("timelite");
 
 //Wo liegen die Dateien fuer die JSON Infos erzeugt werden sollen und welcher Modus soll ausgewertet werden?
 const config = fs.readJSONSync("config.json");
@@ -13,6 +13,7 @@ const mode = config.mode;
 
 //Benennung des Titels
 naming = [];
+naming["bebl"] = "Benjamin Blümchen - ";
 naming["conni"] = "Conni - ";
 naming["bibi"] = "Bibi Blocksberg - ";
 naming["bibi-tina"] = "Bibi und Tina - ";
@@ -35,77 +36,75 @@ const durationPromises = [];
 
 //Dateien aus Video-Ordner holen
 fs.readdir(mediaDir, (err, files) => {
-
-    //Ueber Dateien gehen
-    for (const file of files) {
-
-        //Unsichtbare Datei uebergehen
-        if (file === "System Volume Information") {
-            continue;
-        }
-
-        //Wenn es eine Datei ist und zum aktuellen Modus gehoert
-        const stat = fs.statSync(mediaDir + "/" + file);
-        if (stat && stat.isFile() && file.startsWith(config.mode)) {
-
-            //Promises sammeln, da Zeit-Ermittlung asynchron laeuft
-            durationPromises.push(new Promise((resolve, reject) => {
-
-                //Laenge errechnen fuer Datei
-                getVideoDurationInSeconds(mediaDir + "/" + file).then((duration) => {
-
-                    //Gesamtzeit als formattierten String. Zunaechst Float zu int: 13.4323 => 13
-                    let totalSeconds = Math.trunc(duration);
-
-                    //Umrechung der Sekunden in [h, m, s] fuer formattierte Darstellung
-                    const hours = Math.floor(totalSeconds / 3600);
-                    totalSeconds %= 3600;
-                    const minutes = Math.floor(totalSeconds / 60);
-                    const seconds = totalSeconds % 60;
-
-                    //h, m, s-Werte in Array packen
-                    const timeOutput = [hours, minutes, seconds];
-
-                    //[2,44,1] => 02:44:01
-                    const timeOutputString = timelite.time.str(timeOutput);
-
-                    //bibi-mamis-neuer-besen.mp4 -> mamis-neuer-besen
-                    let name = file.replace(mode + "-", "");
-                    name = name.replace(".mp4", "");
-
-                    //mamis-neuer-besen -> mamis neuer besen
-                    name = name.replace(/-/g, ' ');
-
-                    //mamis neuer besen -> Bibi Blocksberg - mamis neuer besen
-                    name = naming[mode] ? naming[mode] + name : " - ";
-
-                    //Bibi Blocksberg - mamis neuer besen -> Bibi Blocksberg - Mamis Neuer Besen
-                    name = name.replace(/\b[a-z]/g, (chr) => {
-                        return chr.toUpperCase();
-                    });
-
-                    //Video-Objekt erstellen und sammeln
-                    outputArray.push({
-                        "name": name,
-                        "file": file,
-                        "length": timeOutputString,
-                        "added": new Date().toISOString().slice(0, 10)
-                    });
-                    resolve();
-                });
-            }));
-        }
+  //Ueber Dateien gehen
+  for (const file of files) {
+    //Unsichtbare Datei uebergehen
+    if (file === "System Volume Information") {
+      continue;
     }
 
-    //warten bis alle Promises abgeschlossen sind
-    Promise.all(durationPromises).then(() => {
+    //Wenn es eine Datei ist und zum aktuellen Modus gehoert
+    const stat = fs.statSync(mediaDir + "/" + file);
+    if (stat && stat.isFile() && file.startsWith(config.mode)) {
+      //Promises sammeln, da Zeit-Ermittlung asynchron laeuft
+      durationPromises.push(
+        new Promise((resolve, reject) => {
+          //Laenge errechnen fuer Datei
+          getVideoDurationInSeconds(mediaDir + "/" + file).then((duration) => {
+            //Gesamtzeit als formattierten String. Zunaechst Float zu int: 13.4323 => 13
+            let totalSeconds = Math.trunc(duration);
 
-        //Liste nach Dateiname (pippi-01-fest.mp4, pippi-02-freunde.mp4) sortieren, da Promises unterschiedlich schnell zureuckkommen koennen
-        outputArray.sort((a, b) => a.file.localeCompare(b.file));
+            //Umrechung der Sekunden in [h, m, s] fuer formattierte Darstellung
+            const hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+            const minutes = Math.floor(totalSeconds / 60);
+            const seconds = totalSeconds % 60;
 
-        //Video-Array ausgeben
-        console.log(JSON.stringify(outputArray, null, 4));
-    }).catch((err) => {
-        console.log('error:', err);
+            //h, m, s-Werte in Array packen
+            const timeOutput = [hours, minutes, seconds];
+
+            //[2,44,1] => 02:44:01
+            const timeOutputString = timelite.time.str(timeOutput);
+
+            //bibi-mamis-neuer-besen.mp4 -> mamis-neuer-besen
+            let name = file.replace(mode + "-", "");
+            name = name.replace(".mp4", "");
+
+            //mamis-neuer-besen -> mamis neuer besen
+            name = name.replace(/-/g, " ");
+
+            //mamis neuer besen -> Bibi Blocksberg - mamis neuer besen
+            name = naming[mode] ? naming[mode] + name : " - ";
+
+            //Bibi Blocksberg - mamis neuer besen -> Bibi Blocksberg - Mamis Neuer Besen
+            name = name.replace(/\b[a-z]/g, (chr) => {
+              return chr.toUpperCase();
+            });
+
+            //Video-Objekt erstellen und sammeln
+            outputArray.push({
+              name: name,
+              file: file,
+              length: timeOutputString,
+              added: new Date().toISOString().slice(0, 10),
+            });
+            resolve();
+          });
+        })
+      );
+    }
+  }
+
+  //warten bis alle Promises abgeschlossen sind
+  Promise.all(durationPromises)
+    .then(() => {
+      //Liste nach Dateiname (pippi-01-fest.mp4, pippi-02-freunde.mp4) sortieren, da Promises unterschiedlich schnell zureuckkommen koennen
+      outputArray.sort((a, b) => a.file.localeCompare(b.file));
+
+      //Video-Array ausgeben
+      console.log(JSON.stringify(outputArray, null, 4));
+    })
+    .catch((err) => {
+      console.log("error:", err);
     });
 });
--- downloadVideoObject.js
+++ downloadVideoObject.js
@@ -1,10 +1,10 @@
 //Video von Url herunterladen
-const fs = require('fs-extra');
-const download = require('download');
-const { execSync } = require('child_process');
+const fs = require("fs-extra");
+const download = require("download");
+const { execSync } = require("child_process");
 
 //Benennung 003.ts um richtige Reihenfolge sicherzustellen
-const padStart = require('lodash.padstart');
+const padStart = require("lodash.padstart");
 
 //Wo sollen Videos gespeichert werden
 const config = fs.readJSONSync("config.json");
@@ -15,7 +15,7 @@ const source = config.source;
 
 //Dir anlegen, wo fertiges Video liegen soll, falls es nicht existiert
 if (!fs.existsSync(doneDir)) {
-    fs.mkdirSync(doneDir);
+  fs.mkdirSync(doneDir);
 }
 
 //Skript starten
@@ -23,90 +23,103 @@ main();
 
 //Ueber Liste der files gehen, die heruntergeladen werden sollen
 async function main() {
-    for (const video of config.urls) {
-        await downloadVideo(video);
-    }
+  for (const video of config.urls) {
+    await downloadVideo(video);
+  }
 }
 
 //Video aus einzelnen Teilen herunterladen
 async function downloadVideo(video) {
-    console.log("downloading " + video[0]);
+  console.log("downloading " + video[0]);
 
-    //Url je nach Modus aufteilen, damit part-urls erzeugt werden koennen
-    const urlSplit = {
-        zdf: (video[1]).split(/segment\d{1,}/),
-        wdr: (video[1]).split(/segment\d{1,}/),
-        ard: (video[1]).split(/segment\d{1,}/),
-        dm: (video[1]).split(/frag\(\d{1,}/)
-    }[source];
+  //Url je nach Modus aufteilen, damit part-urls erzeugt werden koennen
+  const urlSplit = {
+    zdf: video[1].split(/segment\d{1,}/),
+    wdr: video[1].split(/segment\d{1,}/),
+    ard: video[1].split(/segment\d{1,}/),
+    dm: video[1].split(/frag\(\d{1,}/),
+  }[source];
 
-    //Video-Promises sammeln
-    const videoPromises = [];
+  //Video-Promises sammeln
+  const videoPromises = [];
 
-    //Download-Dir leeren
-    fs.emptyDirSync(downloadDir);
+  //Download-Dir leeren
+  fs.emptyDirSync(downloadDir);
 
-    //Wie viele Segmente sollen fuer diesen Modus abgefragt werden
-    const limit = {
-        zdf: 165,
-        wdr: 12,
-        ard: 45,
-        dm: 500
-    }[source];
+  //Wie viele Segmente sollen fuer diesen Modus abgefragt werden
+  const limit = {
+    zdf: 165,
+    wdr: 12,
+    ard: 45,
+    dm: 500,
+  }[source];
 
-    //Einzelne Teile herunterladen
-    for (let i = 1; i <= limit; i++) {
+  //Einzelne Teile herunterladen
+  for (let i = 1; i <= limit; i++) {
+    //Videos-Promises sammeln
+    videoPromises.push(
+      new Promise((resolve, reject) => {
+        //Url mit part-id erstellen fuer best. Modus
+        const partUrl = {
+          zdf: urlSplit[0] + "segment" + i + urlSplit[1],
+          wdr: urlSplit[0] + "segment" + i + urlSplit[1],
+          ard: urlSplit[0] + "segment" + i + urlSplit[1],
+          dm: urlSplit[0] + "frag(" + i + urlSplit[1],
+        }[source];
 
-        //Videos-Promises sammeln
-        videoPromises.push(new Promise((resolve, reject) => {
+        //Download
+        download(partUrl).then(
+          (data) => {
+            //Datei speichern
+            fs.writeFileSync(
+              downloadDir + "/" + padStart(i, 3, "0") + ".ts",
+              data
+            );
+            resolve();
+          },
+          (err) => {
+            //Bei Fehler trotzdem resolven
+            console.log(err.statusCode);
+            resolve();
+          }
+        );
+      })
+    );
+  }
 
-            //Url mit part-id erstellen fuer best. Modus
-            const partUrl = {
-                zdf: urlSplit[0] + "segment" + i + urlSplit[1],
-                wdr: urlSplit[0] + "segment" + i + urlSplit[1],
-                ard: urlSplit[0] + "segment" + i + urlSplit[1],
-                dm: urlSplit[0] + "frag(" + i + urlSplit[1]
-            }[source];
+  //Wenn alle Dateien heruntergeladen wurden
+  await Promise.all(videoPromises);
+  console.log("download done");
 
-            //Download
-            download(partUrl).then(data => {
+  //in Download-Verzeichnis gehen und ts Dateien zu einer ts-Datei zusammenfuehren
+  switch (config.os) {
+    //Befehl unter Windows
+    case "windows":
+      execSync("cd " + downloadDir + " && copy /b *.ts joined_files.ts");
+      console.log("putting single files together done");
+      break;
 
-                //Datei speichern
-                fs.writeFileSync(downloadDir + "/" + padStart(i, 3, "0") + ".ts", data);
-                resolve();
-            }, err => {
+    //Befehl unter Linux
+    case "linux":
+      execSync("cd " + downloadDir + " && cat *.ts > joined_files.ts");
+      console.log("putting single files together done");
+  }
 
-                //Bei Fehler trotzdem resolven
-                console.log(err.statusCode);
-                resolve();
-            });
-        }));
-    }
+  //ts-Datei nach mp4 konvertieren
+  execSync(
+    "ffmpeg -y -loglevel panic -i " +
+      downloadDir +
+      "/joined_files.ts -acodec copy -vcodec copy " +
+      doneDir +
+      "/" +
+      config.mode +
+      "-" +
+      video[0] +
+      ".mp4"
+  );
+  console.log("creating mp4 file done");
 
-    //Wenn alle Dateien heruntergeladen wurden
-    await Promise.all(videoPromises);
-    console.log("download done");
-
-    //in Download-Verzeichnis gehen und ts Dateien zu einer ts-Datei zusammenfuehren
-    switch (config.os) {
-
-        //Befehl unter Windows
-        case "windows":
-            execSync("cd " + downloadDir + " && copy /b *.ts joined_files.ts");
-            console.log("putting single files together done");
-            break;
-
-        //Befehl unter Linux
-        case "linux":
-            execSync("cd " + downloadDir + " && cat *.ts > joined_files.ts");
-            console.log("putting single files together done");
-    }
-
-    //ts-Datei nach mp4 konvertieren
-    execSync("ffmpeg -loglevel panic -i " + downloadDir + "/joined_files.ts -acodec copy -vcodec copy " + doneDir + "/" + config.mode + "-" + video[0] + ".mp4");
-    console.log("creating mp4 file done");
-
-    //Download-Dir leeren
-    fs.emptyDirSync(downloadDir);
-    console.log("removing downloaded files done");
+  //Download-Dir leeren
+  fs.emptyDirSync(downloadDir);
+  console.log("removing downloaded files done");
 }
